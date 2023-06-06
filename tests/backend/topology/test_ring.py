87eda844884d46e98db768de128f9517dc32dbd3
Mikaela Farrugia <mfarrugi@ND.EDU>
2023-06-06 15:44:38 -0400

    ADD: constrained support in test_ring

--- tests/backend/topology/test_ring.py
+++ tests/backend/topology/test_ring.py
@@ -40,3 +40,41 @@ class TestRingTopology(ABCTestTopology):
         assert (pos[np.argmin(cost)] == pytest.approx(expected_pos)) or (
             pos[np.argmin(cost)] == pytest.approx(expected_pos_2)
         )
+
+    @pytest.mark.parametrize("static", [True, False])
+    @pytest.mark.parametrize("k", [i for i in range(1, 10)])
+    @pytest.mark.parametrize("p", [1, 2])
+    def test_compute_gbest_return_values_(self, constrained_swarm, topology, p, k, static):
+        """Test if update_gbest_neighborhood gives the expected return values"""
+        topo = topology(static=static)
+        pos, cost = topo.compute_gbest(constrained_swarm, p=p, k=k)
+        expected_cost = 1.0002528364353296
+        expected_pos = np.array(
+            [9.90438476e-01, 2.50379538e-03, 1.87405987e-05]
+        )
+        expected_pos_2 = np.array(
+            [9.98033031e-01, 4.97392619e-03, 3.07726256e-03]
+        )
+        assert cost == pytest.approx(expected_cost)
+        assert (pos[np.argmin(cost)] == pytest.approx(expected_pos)) or (
+            pos[np.argmin(cost)] == pytest.approx(expected_pos_2)
+        )
+
+    @pytest.mark.parametrize("static", [True, False])
+    @pytest.mark.parametrize("k", [i for i in range(1, 10)])
+    @pytest.mark.parametrize("p", [1, 2])
+    def test_compute_gbest_violation_return_values_(self, constrained_swarm, topology, p, k, static):
+        """Test if update_gbest_neighborhood gives the expected return values"""
+        topo = topology(static=static)
+        pos, cost = topo.compute_gbest_violation(constrained_swarm, p=p, k=k)
+        expected_cost = -6.0
+        expected_pos = np.array(
+            [9.93740665e-01, -6.16501403e-03, -1.46096578e-02]
+        )
+        expected_pos_2 = np.array(
+            [10.98033031e-01, 5.96392619e-03, 4.07726256e-03]
+        )
+        assert cost == pytest.approx(expected_cost)
+        assert (pos[np.argmin(cost)] == pytest.approx(expected_pos)) or (
+            pos[np.argmin(cost)] == pytest.approx(expected_pos_2)
+        )    
