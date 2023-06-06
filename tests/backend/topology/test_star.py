c31f4d9214bd7e8b8e5897427a59c593c0d893df
Mikaela Farrugia <mfarrugi@ND.EDU>
2023-06-06 15:49:15 -0400

    ADD: constrained support in test_star

--- tests/backend/topology/test_star.py
+++ tests/backend/topology/test_star.py
@@ -32,3 +32,26 @@ class TestStarTopology(ABCTestTopology):
         pos, cost = topo.compute_gbest(swarm, **options)
         assert cost == pytest.approx(expected_cost)
         assert pos == pytest.approx(expected_pos)
+
+    def test_compute_gbest_return_values_(self, constrained_swarm, options, topology):
+        """Test if compute_gbest() gives the expected return values"""
+        topo = topology()
+        expected_cost = 1.0002528364353296
+        expected_pos = np.array(
+            [9.90438476e-01, 2.50379538e-03, 1.87405987e-05]
+        )
+        pos, cost = topo.compute_gbest(constrained_swarm, **options)
+        assert cost == pytest.approx(expected_cost)
+        assert pos == pytest.approx(expected_pos)
+
+    def test_compute_gbest_violation_return_values(self, constrained_swarm, options, topology):
+        """Test if compute_gbest() gives the expected return values"""
+        topo = topology()
+        expected_cost = -6.0
+        expected_pos = np.array(
+            [9.93740665e-01, -6.16501403e-03, -1.46096578e-02]
+        )
+        pos, cost = topo.compute_gbest_violation(constrained_swarm, **options)
+        assert cost == pytest.approx(expected_cost)
+        assert pos == pytest.approx(expected_pos)
+
