62d0a7549472c6a2044873a803516b6d22cffabf
Mikaela Farrugia <mfarrugi@ND.EDU>
2023-06-06 14:32:49 -0400

    ADD: constrained_swarm test_pyramid expansion

--- tests/backend/topology/test_pyramid.py
+++ tests/backend/topology/test_pyramid.py
@@ -34,3 +34,17 @@ class TestPyramidTopology(ABCTestTopology):
         pos, cost = topo.compute_gbest(swarm, **options)
         assert cost == pytest.approx(expected_cost)
         assert pos[np.argmin(cost)] == pytest.approx(expected_pos)
+
+    @pytest.mark.parametrize("static", [True, False])
+    def test_compute_gbest_violation_return_values(
+        self, constrained_swarm, topology, options, static
+    ):
+        """Test if compute_gbest() gives the expected return values"""
+        topo = topology(static=static)
+        expected_violation = -6
+        expected_pos = np.array(
+            [9.93740665e-01, -6.16501403e-03, -1.46096578e-02]
+        )
+        pos, cost = topo.compute_gbest_violation(constrained_swarm, **options)
+        assert cost == pytest.approx(expected_violation)
+        assert pos[np.argmin(cost)] == pytest.approx(expected_pos)
